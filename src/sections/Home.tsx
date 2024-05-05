4e6b44d83a0a7f570e1307cdd8d73a011380d234
Lucas Ribeiro Bonfílio de Lemos <lucasrblemos@gmail.com>
2024-05-05 13:34:24 -0300

    Modify Home

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,51 +1,73 @@
+import { SetStateAction, useState } from "react";
 import { Table } from "../components/Table/Table";
 import { TableCell } from "../components/Table/TableCell";
 import { TableHeader } from "../components/Table/TableHeader";
 import { TableRow } from "../components/Table/TableRow";
-
+import { ChevronsLeft, ChevronLeft, ChevronRight, ChevronsRight } from 'lucide-react';
+import { IconButton } from "../components/Table/IconButton";
 
 
 const Home = () => {
   const array = [
     {id: 1, name: 'José', turma: '3', empresa: 'Nestlé'},
     {id: 2, name: 'José', turma: '3', empresa: 'Nestlé'},
-    {id: 3, name: 'José', turma: '3', empresa: 'Nestlé'}
+    {id: 3, name: 'José', turma: '3', empresa: 'Nestlé'},
+    {id: 4, name: 'José', turma: '3', empresa: 'Nestlé'},
+    {id: 5, name: 'José', turma: '3', empresa: 'Nestlé'},
+    {id: 6, name: 'José', turma: '3', empresa: 'Nestlé'},
+    {id: 7, name: 'José', turma: '3', empresa: 'Nestlé'},
+    {id: 8, name: 'José', turma: '3', empresa: 'Nestlé'},
+    {id: 9, name: 'José', turma: '3', empresa: 'Nestlé'},
+    {id: 10, name: 'José', turma: '3', empresa: 'Nestlé'},
+    {id: 11, name: 'José', turma: '3', empresa: 'Nestlé'},
+    {id: 12, name: 'José', turma: '3', empresa: 'Nestlé'}
   ]
 
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const pageSize = 5;
+  const filteredObj = array.filter(item => item.name.toLowerCase().includes(search))
 
-  // function setCurrentPage(page: number) {
-  //   const url = new URL(window.location.toString());
-  //   url.searchParams.set('page', String(page));
-  //   window.history.pushState({}, '', url);
-  //   setPage(page);
-  // }
+  const totalPages = Math.ceil(filteredObj.length / pageSize);
 
-  // function onSearchInputChange(ev: ChangeEvent<HTMLInputElement>) {
-  //   setCurrentSearch(ev.target.value);
-  //   setCurrentPage(1);
-  // }
 
-  // function goToNextPage() {
-  //   setCurrentPage(page + 1);
-  // }
+  const handleNextPage = () => {
+    if(page < totalPages) {
+      setPage(page + 1)
+    }
+   }
 
-  // function goToPreviousPage() {
-  //   setCurrentPage(page - 1);
-  // }
+   const handlePrevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
 
-  // function goToFirstPage() {
-  //   setCurrentPage(1);
-  // }
+  const handleFirstPage = () => {
+    setPage(1);
+  };
 
-  // function goToLastPage() {
-  //   setCurrentPage(totalPages);
-  // }
+  const handleLastPage = () => {
+    setPage(totalPages);
+  };
+
+  const handleSearchChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setSearch(event.target.value);
+    setPage(1); // Reset page to 1 when search query changes
+  };
 
 
   return (
     <div className="w-full h-screen bg-[#141627] flex flex-col justify-center items-center">
       <div className="bg-[#000000] w-full h-20"></div>
       <div className="m-auto">
+      <input
+        type="text"
+        value={search}
+        onChange={handleSearchChange}
+        placeholder="Search..."
+      />
+
         <Table>
           <thead>
             <tr className="border-b border-white/10">
@@ -78,13 +100,27 @@ const Home = () => {
           <tfoot>
             <tr>
               <TableCell colSpan={3}>
-                  Teste
+                  {array.length}
               </TableCell>
               <TableCell className="text-right" colSpan={3}>
                 <div className="inline-flex items-center gap-8">
-                  <span>Teste</span>
+                  <span>Página {page} de {totalPages}</span>
                 </div>
-              </TableCell>
+                <div>
+                  <IconButton onClick={handleFirstPage} disabled={page === 1}>
+                      <ChevronsLeft className="size-4" />
+                  </IconButton>
+                  <IconButton onClick={handlePrevPage} disabled={page === 1}>
+                      <ChevronLeft className="size-4" />
+                  </IconButton>
+                  <IconButton onClick={handleNextPage} disabled={page === totalPages}>
+                    <ChevronRight className="size-4" />
+                  </IconButton>
+                  <IconButton onClick={handleLastPage} disabled={page === totalPages}>
+                    <ChevronsRight className="size-4" />
+                  </IconButton>
+      </div>
+          </TableCell>
             </tr>
           </tfoot>
         </Table>
