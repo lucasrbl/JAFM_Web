eb516f0be7dd93a4345ed7d7c6c2a95d857f554c
Lucas Ribeiro Bonfílio de Lemos <lucasrblemos@gmail.com>
2024-05-24 20:53:10 -0300

    LP redirect to Login

--- src/sections/LandingPage.tsx
+++ src/sections/LandingPage.tsx
@@ -25,7 +25,7 @@ const LandingPage = () => {
           <div className="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link page-scroll text-red" href="#header">Home<span className="sr-only">(current)</span></a>
+                <a className="nav-link page-scroll text-red" href="/login">Home<span className="sr-only">(current)</span></a>
               </li>
             </ul>
           </div>
