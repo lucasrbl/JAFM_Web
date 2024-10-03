5baa4305fe7be68dd512bc335c086640d98d68f3
Lucas Ribeiro Bonfilio de Lemos <lucasrblemos@gmail.com>
2024-10-03 20:26:48 -0300

    feat: Add conditional statements for rendering responsive pages

--- src/pages/Templates/Template.tsx
+++ src/pages/Templates/Template.tsx
@@ -1,12 +1,27 @@
 import { Outlet } from 'react-router-dom';
 import { Logo } from './components/Logo/Logo';
 
+import { useGlobalContext } from '@/context/useGlobalContext';
+import { If } from '@/components/If/If';
+
 const Template = () => {
+  const { isMobile } = useGlobalContext();
+
   return (
-    <div className='template'>
-      <Logo />
-      <Outlet />
-    </div>
+    <section>
+      <If condition={isMobile}>
+        <div className='md:hidden'>
+          <Outlet />
+        </div>
+      </If>
+
+      <If condition={!isMobile}>
+        <div className='template hidden md:flex'>
+          <Logo />
+          <Outlet />
+        </div>
+      </If>
+    </section>
   );
 };
 
